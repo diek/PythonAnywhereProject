9f8e11ab1de2ee5dc44ff43435ad7f5b04efc1f2
diek <d_kearney@bellaliant.net>
2021-08-20 20:29:19 -0300

    Update ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['diek.pythonanywhere.com']
+ALLOWED_HOSTS = ['diek.pythonanywhere.com', '*']
 
 
 # Application definition
